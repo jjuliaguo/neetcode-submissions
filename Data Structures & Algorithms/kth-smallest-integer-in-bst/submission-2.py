c9c3a7c3e41a3078ad49bc17a0e66dbb139fd263
Julia Guo <86933675+jjuliaguo@users.noreply.github.com>
2026-06-20 15:26:55 -0400

    Add: kth-smallest-integer-in-bst - submission-2

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-2.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        #don't u just do dfs and get an array
+        #make sure to append the Null ones too
+        #then return the number at that index?
+
+        result = []
+
+        def dfs(node):
+            #this checks if its a leaf
+            #if not node.left and not node.right:
+            if not node: #checks if this node exists
+                #this check is redundant, if 
+                # if node == None: #should be == not !=
+                    return node
+            
+            dfs(node.left) #u can assume that the result.append handles the adding to list
+            result.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        return result[k-1] #u dont need to add the Nones into list anyway
+        #the nulls are in the input to help u understand what the tree looks like
