6e667f506080efcafd097f1c72298d56ba33d27d
Julia Guo <86933675+jjuliaguo@users.noreply.github.com>
2026-09-11 18:41:59 -0400

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        #use binary search for every row
+        #if the right index reaches the column length-1 then increment the row
+        
+        end = len(matrix)-1
+        for row in range(len(matrix)):
+            l, r = 0, len(matrix[0]) - 1
+            while l <= r:
+                mid = (l+r)//2 
+                if matrix[row][mid] < target:
+                    l = mid +1 
+                elif matrix[row][mid] > target:
+                    r = mid - 1
+                else:
+                    return True
+
+        return False
